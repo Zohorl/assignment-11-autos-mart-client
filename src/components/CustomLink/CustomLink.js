fc9824ae46345120cd49770d4a2c23aad1e2daeb
Zohorl <zohorul109561@gmail.com>
2022-04-30 12:39:19 +0600

    custom link added

---
+++ src/components/CustomLink/CustomLink.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
+
+const CustomLink = ({ children, to, ...props }) => {
+
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+
+    return (
+        <div>
+            <Link
+                style={{ fontWeight: match ? 'bold' : '', color: match ? "orange" : "white", textDecoration: match ? "none" : "none" }}
+                to={to}
+                {...props}
+            >
+                {children}
+            </Link>
+            {match && ""}
+        </div>
+    );
+};
+
+export default CustomLink;
